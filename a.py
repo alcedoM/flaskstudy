e88387aa9a8c86e042a515ec3b4584cb8f0d864b
alcedoM <1323725572@qq.com>
2021-03-23 13:19:20 +0800

    Add base template

--- a.py
+++
@@ -1,9 +0,0 @@
-import click
-@app.cli.command()
-@click.option('--drop', is_flag_True, help='Create after drop.')
-def initdb(drop):
-    """Initlialize the database"""
-    if drop:
-        db.drop_all()
-    db.create_all()
-    click.echo('Initialized database.')
--- app.py
+++ app.py
@@ -3,9 +3,33 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
+
+#flask 页面
+#上下文处理
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user = user)
+
+# 主页面
+@app.route('/')
+def index():
+    movies = Movie.query.all()
+    return render_template('index.html', movies=movies)
+
+@app.route('/user/<name>')
+def user_page(name):
+    return 'User: %s' % name
+
+# 错误页面装饰
+@app.errorhandler(404)
+def page_not_find(e):
+    user = User.query.first()
+    return render_template('404.html'), 404
+
+# 数据库主体
 db = SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///' + os.path.join(app.root_path, 'data.db')
-
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20))
@@ -13,7 +37,7 @@ class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(60))
     year = db.Column(db.String(4))
-
+    
 #数据库更新
 import click
 @app.cli.command() 
@@ -25,29 +49,6 @@ def initdb(drop):
     db.create_all()
     click.echo('Initialized database.')
 
-#flask 页面
-@app.route('/')
-def index():
-    user = User.query.first()
-    movie = Movie.query.all()
-    
-    name = 'Grey Li'
-    movies = [
-        {'title': 'My Neighbor Totoro', 'year': '1988'},
-        {'title': 'Dead Poets Society', 'year': '1989'},
-        {'title': 'A Perfect World', 'year': '1993'},
-        {'title': 'Leon', 'year': '1994'},
-        {'title': 'Mahjong', 'year': '1996'},
-        {'title': 'Swallowtail Butterfly', 'year': '1996'},
-        {'title': 'King of Comedy', 'year': '1999'},
-        {'title': 'Devils on the Doorstep', 'year': '1999'},
-        {'title': 'WALL-E', 'year': '2008'},
-        {'title': 'The Pork of Music', 'year': '2012'},
-     ]
-    return render_template('index.html', name=user.name, movies=movie)
 
-@app.route('/user/<name>')
-def user_page(name):
-    return 'User: %s' % name
 
 
