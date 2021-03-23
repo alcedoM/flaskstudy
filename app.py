c9745f292fe818da849ee1db84fa1cbe8185caee
alcedoM <1323725572@qq.com>
2021-03-23 22:51:12 +0800

    edit,create

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, request, url_for, render_template, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'dev'
 #flask 页面
 #上下文处理
 @app.context_processor
@@ -12,10 +12,48 @@ def inject_user():
     return dict(user = user)
 
 # 主页面
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        year = request.form.get('year')
+        if not title or not year or len(year) > 4 or len(title)>60:
+            flash('Invalid input.')
+            return redirect(url_for('index'))
+        movie = Movie(title = title, year=year)
+        db.session.add(movie)
+        db.session.commit()
+        flash('Item created.')
+        return redirect(url_for('index'))
+
     movies = Movie.query.all()
     return render_template('index.html', movies=movies)
+
+#编辑信息
+@app.route('/movie/edit/<int:movie_id>',methods=['GET','POST'])
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    if request.method == 'POST':
+        title = request.form['title']
+        year = request.form['year']
+        if not title or not year or len(year)>4 or len(title)>60:
+            flash('Invalid input')
+            return redirect(url_for('edit',movie_id=movie_id))
+        movie.title = title
+        movie.year = year
+        db.session.commit()
+        flash('updated')
+        return redirect(url_for('index'))
+    return render_template('edit.html',movie=movie)
+
+#删除
+@app.route('/movie/delete/<int:movie_id>',methods=['POST'])
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    flash('Item deleted')
+    return redirect(url_for('index'))
 
 @app.route('/user/<name>')
 def user_page(name):
